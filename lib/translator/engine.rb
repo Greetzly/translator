a130f03bc0df2c25d0cfae254a0da4e855153e83
Kamil Dziemianowicz <dziemian_rec@wp.pl>
2013-12-19 10:24:34 +0100

    Include gem assets in rails engine to serve feom assets path

--- lib/translator/engine.rb
+++ lib/translator/engine.rb
@@ -1,5 +1,15 @@
 module Translator
   class Engine < Rails::Engine
     isolate_namespace Translator
+    initializer 'static_assets.load_static_assets' do |app|
+      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/app")
+
+      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+
+      app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/vendor")
+    end
   end
 end
